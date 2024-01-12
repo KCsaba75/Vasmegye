a1ad4c09b72677fdfc6a67e20ab1e64fd3856b18
KCsaba75 <app.fejleszt@gmail.com>
2024-01-12 10:11:17 +0100

    Első commit!

---
+++ Vasmegye/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // file olvasáa miatt kell.
+
+namespace Vasmegye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. feladat: Adatok beolvasása tárolás");
+        }
+    }
+}
